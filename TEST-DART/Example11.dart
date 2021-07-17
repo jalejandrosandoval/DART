b90ce6771056048a1fe82cf759e595538f092695
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2021-07-17 10:12:56 -0500

    Modificación

--- TEST-DART/Example11.dart
+++ TEST-DART/Example11.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 main(){
 
-  Operators2();
+  Operators6();
 
 }
 
@@ -28,5 +28,56 @@ Operators2(){
   for(var i=0; i<_a ;i++){
     print("data $_a");
   }
+
+}
+
+Operators3(){
+
+  stdout.writeln("...");
+
+  int base = int.parse(stdin.readLineSync()!);
+
+  for(var i =0; i<10; i++){
+    stdout.writeln("Base: $base * $i = ${i*base}");
+  }
+
+}
+
+Operators4(){
+
+  List<String> _List = ["_data1", "_data2", "_data3"];
+
+  for(var i =0; i<_List.length; i++){
+    print("$i " + _List[i]);
+  }
+
+  for(String _var in _List){
+    print(_var);
+  }
+}
+
+Operators5(){
+
+  while(true){
+    print("Test");
+  }
+
+}
+
+Operators6(){
+
+  String _option = "y";
+  var cont = 0;
+
+  while(_option == "y"){
+    cont++;
+
+    stdout.writeln("$cont");
+
+    stdout.writeln("Opcion_ y/n");
+    
+    _option = stdin.readLineSync() ?? "n";
+  }
+
 
 }
