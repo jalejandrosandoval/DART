df2fd17f854677625d9abaf3230147618bc0827c
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2021-07-17 08:11:02 -0500

    Séptimo Ejemplo: Declaración de clases

---
+++ TEST-DART/Example7.dart
@@ -0,0 +1,16 @@
+main(){
+  
+  TestClass _test = new TestClass();
+  _test.Name = "Nombres";
+
+  _test._print();
+
+}
+
+class TestClass{
+  String Name = "";
+  
+  void _print(){
+    print(Name);  
+  }
+}
