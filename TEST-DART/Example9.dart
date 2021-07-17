c7c3101b2151b412db50e6f390216f049e8a9be7
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2021-07-17 08:45:46 -0500

    Noveno Ejemplo: Operadores

---
+++ TEST-DART/Example9.dart
@@ -0,0 +1,46 @@
+void main(){
+
+  //Operadores de Asignación
+
+  int a = 20;
+  String _data = a > 18 ? "Mayor a 18" : "Menor a 18";
+
+  print(_data);
+
+  //Operadores Condicionales
+
+  int _a = 10;
+  int? _b;
+
+  int _c = _b ?? a;
+  print(_c);
+
+  //Operadores Relacionales
+
+  OperadoresRelaciones();
+
+}
+
+OperadoresRelaciones(){
+
+  int a = 2021;
+  int b = 848;
+  
+  String _data1 = "Nombres1";
+  String _data2 = "Nombres2"; 
+
+  int y = 2021;
+  String z = "2021";
+
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+
+  print(_data1 == _data2);
+  print(_data1 != _data2);
+
+  print(y is int);
+  print(z is int);
+
+}
