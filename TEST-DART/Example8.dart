a23b112397e2ce7074c0487abbccee8c957657c1
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2021-07-17 08:34:31 -0500

    Octavo Ejemplo: Operadores matematicos

---
+++ TEST-DART/Example8.dart
@@ -0,0 +1,23 @@
+void main(){
+
+/*
+  final int _fint = 10; // No cambia su valor, a menos de una lista o array 
+  const int _cint = 10; //
+  */
+
+  //Operadores
+
+  int a = 1;
+  a = 2 + 2;
+  a = 2 - 1;
+  print(a);
+
+  double b = 1/2;
+  b = 1 % 2;
+  print(b);
+
+  //Parte Entera Cociente
+  int c = 1 ~/ 2;
+  print(c);
+
+}
