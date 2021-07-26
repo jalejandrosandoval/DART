b570e4e2bb8b75be802b7b7789a7d760fddbbb87
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2021-07-26 11:07:54 -0500

    Ejemplo de clases 2

---
+++ TEST-DART-FLUTTER/PROJECT-EXAMPLE/CLASSES/Class_Second.dart
@@ -0,0 +1,10 @@
+class Class_Second{
+
+  final int side;
+  final int area;
+
+  Class_Second(int _side):
+    this.side = _side,
+    this.area = _side * _side;  
+  
+}
---
+++ TEST-DART-FLUTTER/PROJECT-EXAMPLE/main_class_second.dart
@@ -0,0 +1,6 @@
+import 'CLASSES/Class_Second.dart';
+
+main(){
+  final _data = new Class_Second(20);
+  print(_data);
+}
